1931547d51282be4ba9db7804903130f8334840b
Luis Antonio Rodríguez Elías <67299425+toni310@users.noreply.github.com>
2021-03-18 17:59:25 -0600

    Interfaz para video 1 de diseño
    Se creo la interfaz para el video uno de Diseño

---
+++ src/views/DisenoVideo1.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Diseño: Episodio 1</h1>
+  <div class="video">
+      <div class="embed-responsive embed-responsive-21by9">
+      <h2>Video</h2>
+        <iframe class="embed-responsive-item" src="https://www.youtube.com/watch?v=rblYSKz_VnI" allowfullscreen></iframe>
+      </div>
+  </div>
+</template>
+
+<script>
+export default 
+{
+  name: 'DiseñoVideo1',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
