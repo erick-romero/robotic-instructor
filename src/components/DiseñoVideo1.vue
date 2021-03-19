7d1b4221705aa5b44123911c95f71c57f80e0f0d
eduardosalaz <eduardosalaz@outlook.com>
2021-03-19 10:58:26 -0600

    se agrega el video y transcripción

--- src/components/DiseñoVideo1.vue
+++ src/components/DiseñoVideo1.vue
@@ -3,14 +3,35 @@
   <div class="video">
       <div class="embed-responsive embed-responsive-21by9">
       <h2>Video</h2>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/FzifppqSyeE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
-      <h1>ya es toda wey</h1>
+      <div class="container-lg">
+        <h1>Transcripción de la Lección: </h1>
+        <p class="texto">
+          ¿Qué es el diseño 3D?
+          Quizá lo hayas escuchado antes, en las películas, videojuegos, hasta conversaciones casuales. <br>
+          El diseño 3D combina el diseño gráfico, y el diseño industrial, y puede incluso ser considerado como un arte. <br>
+          Podemos diseñar objetos iguales a los que vemos día a día, ya que todos tienen 3 dimensiones. Incluso podemos escanear de diversas formas, para despúes imprimirlos en 3D, pero de esto hablaremos en el próximo video.
+          El diseño 3D abre nuevos mundos, nos permite ayudar a los doctores, a los constructores, a los profesores, o hasta los artistas. <br>
+          En la robótica, quizá ya puedas imaginarte los usos. ¿Puedes ver robots impresos en 3D? <br>
+          Los diseñadores 3D crean imágenes en 3 dimensiones, que luego puede ser usado darle vida a un personaje, en juegos, películas, o incluso crear uno real, como los robots de Disney.
+          Estos diseños se crean en programas especiales, que veremos más adelante. Incluso usaremos uno. <br>
+          El diseño 3D, y la impresión, son tierras muy nuevos que aún hay que explorar. <br>
+          <br>
+          <strong>Nos vemos en el siguiente video.</strong>
+          
+        </p>
+      </div>
+
   </div>
 </template>
 
 <style>
 .video{
   background-color: white;
+}
+.texto{
+  font-size: 25px;
+  text-align:justify;
 }
 </style>
