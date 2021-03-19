5728bc1e337c79ead1fd10d1876e623f0a0e078f
Fersalinas99 <80018869+Fersalinas99@users.noreply.github.com>
2021-03-18 19:13:51 -0600

    program
    programacion

---
+++ src/components/diseño_programacion.vue
@@ -0,0 +1,100 @@
+<template>
+    <h1 style="margin: 50px">Programacion</h1>
+
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <img src="" class="card-img-top" alt="">
+                <div class="card-body align-center">
+                    <h5 class="card-title">Episodio 1</h5>
+                    <p class="card-text">¿Qué es la programacion?</p>
+                    <a href="#" class="btn btn-primary">Ir</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="" class="card-img-top" alt="">
+                <div class="card-body align-center">
+                    <h5 class="card-title">Episodio 2</h5>
+                    <p class="card-text">Usos Generales de la Programacion</p>
+                    <a href="#" class="btn btn-primary">Ir</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="" class="card-img-top" alt="">
+                <div class="card-body align-center">
+                    <h5 class="card-title">Episodio 3</h5>
+                    <p class="card-text">Programacion en Robotica</p>
+                    <a href="#" class="btn btn-primary">Ir</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="" class="card-img-top" alt="">
+                <div class="card-body align-center">
+                    <h5 class="card-title">Episodio 4</h5>
+                    <p class="card-text">Tomar decisiones con programacion</p>
+                    <a href="#" class="btn btn-primary">Ir</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="" class="card-img-top" alt="">
+                <div class="card-body align-center">
+                    <h5 class="card-title">Episodio 5</h5>
+                    <p class="card-text">Software para simular programaciones</p>
+                    <a href="#" class="btn btn-primary">Ir</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</template>
+
+<style scoped>
+
+
+    .card {
+        margin-top: 20px;
+        background-color: #ffffff !important;
+        padding: 40px;
+        /* margin-right: 10px;
+    margin-left: 10px; */
+    }
+
+    .btn {
+        font-family: cooper,arial;
+        font-size: 25px;
+    }
+
+
+    .btn-primary {
+        background-color: #0C6980;
+        border-color: #0C6980;
+        border-radius: 30px;
+        width: 120px;
+    }
+
+    .card-title {
+        font-family: cooper,arial;
+        font-size: 25px;
+    }
+
+    .card-text {
+        font-family: cooper,arial;
+        color: black;
+        height: 70px;
+        text-align: middle;
+    }
+
+    .btn-primary:hover {
+        background-color: #00A8A8;
+        border-color: #00A8A8;
+    }
+</style>
---
+++ src/views/Programacion.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <diseño_programacion />
+    <router-view />
+</template>
+
+
+<script>
+// @ is an alias to /src
+
+import diseño_programacion from '@/components/diseño_programacion.vue'
+
+export default
+{
+  name: 'Programacion',
+  components: {
+    diseño_programacion
+  }
+}
+
+</script>
+
