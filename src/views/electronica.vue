638c3dc395399811166a7d3114bc407aed663d26
Fersalinas99 <80018869+Fersalinas99@users.noreply.github.com>
2021-05-03 22:02:18 -0500

    interfaz electronica
    interfaz electronica

---
+++ src/views/electronica.vue
@@ -0,0 +1,21 @@
+<template>
+  
+  <diseño_electronica/>
+  <router-view/>
+</template>
+
+
+<script>
+// @ is an alias to /src
+
+import diseño_electronica from '@/components/diseño_electronica.vue'
+
+export default 
+{
+  name: 'electronica',
+  components: {
+    diseño_electronica
+  }
+}
+
+</script>
